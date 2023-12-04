ef615116edace77647375b96ff4dc718d7c10ad5
msg2324 <igiza1213@gmail.com>
2023-12-04 18:58:41 +0900

    dd

--- JavaScript/Next.js/notion-clone/convex/schema.ts
+++ JavaScript/Next.js/notion-clone/convex/schema.ts
@@ -2,11 +2,11 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  document: defineTable({
+  documents: defineTable({
     title: v.string(),
     userId: v.string(),
     isArchived: v.boolean(),
-    parentDocument: v.optional(v.id("document")),
+    parentDocument: v.optional(v.id("documents")),
     content: v.optional(v.string()),
     coverImage: v.optional(v.string()),
     icon: v.optional(v.string()),
