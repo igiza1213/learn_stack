85bf5ded06b0025d05e18064020b671d7ec9d16a
igiza1213 <igiza1213@gmail.com>
2024-01-04 15:31:21 +0900

    c

--- JavaScript/Next.js/my-app/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Component } from "@/components/component/component";
-
-export default function Home() {
-  return <Component />;
-}
--- JavaScript/Next.js/notion-clone/app/(main)/_components/navigation.tsx
+++ JavaScript/Next.js/notion-clone/app/(main)/_components/navigation.tsx
@@ -126,7 +126,7 @@ const Navigation = () => {
           onClick={collapse}
           role="button"
           className={cn(
-            "h-6 w-6 text-text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
+            "h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
             isMobile && "opacity-100"
           )}
         >
