5047df25ed1b11a6fa02ce0be36d228bf8e6bd06
igiza1213 <igiza1213@gmail.com>
2024-01-16 21:16:31 +0900

    c

--- JavaScript/Next.js/x-com/src/app/(afterLogin)/_component/Post.tsx
+++ JavaScript/Next.js/x-com/src/app/(afterLogin)/_component/Post.tsx
@@ -3,14 +3,20 @@ import Link from "next/link";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ko";
-import ActionButtons from "./ActionButtons";
-// import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import ActionButtons from "@/app/(afterLogin)/_component/ActionButtons";
+import PostArticle from "@/app/(afterLogin)/_component/PostArticle";
+import { faker } from "@faker-js/faker";
+import PostImages from "@/app/(afterLogin)/_component/PostImages";
 
 dayjs.locale("ko");
 dayjs.extend(relativeTime);
 
-export default function Post() {
+type Props = {
+  noImage?: boolean;
+};
+export default function Post({ noImage }: Props) {
   const target = {
+    postId: 1,
     User: {
       id: "elonmusk",
       nickname: "Elon Musk",
@@ -18,10 +24,19 @@ export default function Post() {
     },
     content: "클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ",
     createdAt: new Date(),
-    Images: [],
+    Images: [] as any[],
   };
+  if (Math.random() > 0.5 && !noImage) {
+    target.Images.push(
+      { imageId: 1, link: faker.image.urlLoremFlickr() },
+      { imageId: 2, link: faker.image.urlLoremFlickr() },
+      { imageId: 3, link: faker.image.urlLoremFlickr() },
+      { imageId: 4, link: faker.image.urlLoremFlickr() }
+    );
+  }
+
   return (
-    <article className={style.post}>
+    <PostArticle post={target}>
       <div className={style.postWrapper}>
         <div className={style.postUserSection}>
           <Link href={`/${target.User.id}`} className={style.postUserImage}>
@@ -42,10 +57,12 @@ export default function Post() {
             </span>
           </div>
           <div>{target.content}</div>
-          <div className={style.postImageSection}></div>
+          <div>
+            <PostImages post={target} />
+          </div>
           <ActionButtons />
         </div>
       </div>
-    </article>
+    </PostArticle>
   );
 }
--- JavaScript/Next.js/x-com/src/app/(afterLogin)/messages/page.tsx
+++ JavaScript/Next.js/x-com/src/app/(afterLogin)/messages/page.tsx
@@ -1,3 +1,18 @@
-export default function Page() {
-  return <main>쪽지페이지</main>;
+import style from "./message.module.css";
+import Room from "@/app/(afterLogin)/messages/_component/Room";
+
+export default function Home() {
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+    </main>
+  );
 }
